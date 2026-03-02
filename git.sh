e3a043549f42b73babd2a0d81ec447a20355e8c4
Francisleon102 <francis.njoku@wsu.edu>
2026-03-02 08:25:51 -0800

    git sh

--- git.sh
+++ git.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 read -p "Enter your commit message: " text
+
 git add .
-git commit -m "$text"
-git push origin main
+
+if git diff --cached --quiet; then
+    echo "Nothing to commit."
+else
+    git commit -m "$text"
+    git push origin main
+fi
